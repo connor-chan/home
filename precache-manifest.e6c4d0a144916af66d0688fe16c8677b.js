049c644bdc2bcd49754f5bb80fb4aec4c0e28e0a
Connor Chan <connorchan@Connors-MacBook-Pro.local>
2022-06-28 22:46:06 -0700

    Updates

---
+++ precache-manifest.e6c4d0a144916af66d0688fe16c8677b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b52d5a73c008d387e203",
+    "url": "/home/static/css/main.01b830ca.chunk.css"
+  },
+  {
+    "revision": "b52d5a73c008d387e203",
+    "url": "/home/static/js/main.b52d5a73.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/home/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "e258ea1747dec9816919",
+    "url": "/home/static/js/runtime~main.e258ea17.js"
+  },
+  {
+    "revision": "776d38094c4be092aa2bfd6582315535",
+    "url": "/home/index.html"
+  }
+];
